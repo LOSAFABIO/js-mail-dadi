a37ec2b87ffd490717bb0ee2f70d3ff60994c60b
LOSAFABIO <fabio.losa95@gmail.com>
2021-12-13 16:48:48 +0100

    Mail fatta

---
+++ Mail/myscript.js
@@ -0,0 +1,24 @@
+let email = prompt( `Email utente` );   // Richiesta email
+
+let listaEmail = []; // Lista email
+
+listaEmail.push( `ciao@gmail.com` );
+listaEmail.push( `amico@gmail.com` );
+listaEmail.push( `fabio@gmail.com` );
+listaEmail.push( `losa@gmail.com` );
+listaEmail.push( `buongiorno@gmail.com` );
+
+let emailControl = false
+
+for ( i = 0 ; i < listaEmail.length ; i++ ){ //ciclo ugualianza email con lista
+    if ( email == listaEmail[i] ){  
+        emailControl = true;
+    }
+}
+
+if( emailControl ){
+    document.writeln( `L'email inserita è presente nella lista.` );
+}else{
+    document.writeln( `L'email inserita non è presente nella lista.` ) ;
+}
+
